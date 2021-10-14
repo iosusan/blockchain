55a877817a1934cd4fc14b98bf1570482afefade
smoke@laptop <iosu.santurtun@wiseathena.com>
2021-10-14 13:20:36 +0200

    added postman

--- web2.0/server.js
+++ web2.0/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.post('/', (req, res)=>{
+// as of express 4.16+ there is no need to import bodyParser
+//const bodyParser = require('body-parser');
+
+/*handling all the parsing */
+app.use(express.json());
+
+
+
+app.post('/', (req, res) => {
   var email = req.body.email;
   var amount = req.body.amount;
+  console.log(email);
+  console.log(amount);
+  res.send({'amount' :amount, 'email': email});
 
-  res.send({"amount":amount, "email": email})
 });
+
+app.post('/test', (req, res) => {
+    res.json({requestBody:req.body})
+})
 
 app.listen(3000, ()=>{
     console.log('server is running on port 3000');
