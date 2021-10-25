98f8e1e7599c16667389ba70ef06551f1376fd2f
smoke@laptop <iosu.santurtun@wiseathena.com>
2021-10-25 07:13:44 +0200

    added mysql conn

---
+++ web2.0/db.js
@@ -0,0 +1,31 @@
+var mysql = require('mysql');
+var dbconfig = {
+    host: '127.0.0.1',
+    user: 'web3test',
+    'password': '#e#eMast3r',
+    database: 'web3test'
+}
+
+var connection;
+
+function handleDisconnect(){
+    connection = mysql.createConnection(dbconfig);
+    connection.connect(function(err){
+        if(err){
+            console.log('error when connecting to database.');
+            setTimeout(handleDisconnect, 2000);
+        }
+    });
+    connection.on('error', function(err){
+        if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+            handleDisconnect();
+        }else{
+            throw err;
+        }
+    })
+
+}
+
+handleDisconnect();
+
+module.exports = connection;
